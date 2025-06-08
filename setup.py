88589a34adfc6ffcd6e4a2cf50d1187219639a08
ManavChauhan123 <manavk638@gmail.com>
2025-06-08 16:27:07 +0530

    logging and exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Manav",
+    author_email="chauhankmv@gmail.com",
+    packages=find_packages()
+)
